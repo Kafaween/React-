590f4c88947b1f2d0aca7447c4b3130420549e00
Kafaween <abod.kafaween@gmail.com>
2021-07-26 02:44:45 +0300

    lab1 done

--- horned-beasts/src/App.js
+++ horned-beasts/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import Main from "./components/main";
+import Footer from "./components/footer";
+import Header from "./components/header";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header title="this is header" />
+        <Main />
+        <Footer author="abdel-rahman kafaween" />
+      </div>
+    );
+  }
 }
 
 export default App;
