93246726b545a92d45c3fe413d2936fa97bbe192
Kafaween <abod.kafaween@gmail.com>
2021-07-28 05:12:58 +0300

    done lab 3

--- horned-beasts/src/App.js
+++ horned-beasts/src/App.js
@@ -6,13 +6,50 @@ import Header from './components/header';
 import data from './components/lab.json';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      title: null,
+      url: null,
+      des: null,
+    };
+  }
+  handledata = (obj) => {
+    this.setState({
+      title: obj.title,
+      url: obj.url,
+      des: obj.des,
+    });
+  };
+  handleShow = () => {
+    this.setState({
+      show: true,
+    });
+  };
+
+  handleHide = () => {
+    this.setState({
+      show: false,
+    });
+  };
   render() {
     return (
       <div>
+        <SelectedBeast
+          show={this.state.show}
+          title={this.state.title}
+          url={this.state.url}
+          handleHide={this.handleHide}
+          des={this.state.des}
+        />
         <Header title='this is header' />
-        <Main d={data} />
+        <Main
+          d={data}
+          handledata={this.handledata}
+          handleShow={this.handleShow}
+        />
         <Footer author='abdel-rahman kafaween' />
-        <SelectedBeast />
       </div>
     );
   }
